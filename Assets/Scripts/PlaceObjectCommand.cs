481a5bb6278d10a70d53e9a0b2697d1a7d309e0e
Loudcow67 <logan.soulliere@ontariotechu.net>
2020-10-31 11:53:06 -0400

    Command Pattern + Respawn

---
+++ Assets/Scripts/PlaceObjectCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceObjectCommand : ICommand
+{
+    Vector3 position;
+    Transform transform;
+
+    public PlaceObjectCommand(Vector3 position, Transform transform)
+    {
+        this.position = position;
+        this.transform = transform;
+    }
+
+    public void Execute()
+    {
+        CubePlacer.placeShape(position, transform);
+    }
+
+    public void Undo()
+    {
+        CubePlacer.removeObject(position);
+    }
+}
