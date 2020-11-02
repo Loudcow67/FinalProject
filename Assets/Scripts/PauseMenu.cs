fbc31b09e7ac158e254000a042019ed0affed3be
Loudcow67 <logan.soulliere@ontariotechu.net>
2020-11-02 09:26:05 -0500

    Save and Load + Pause

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pauseMenuUI;
+
+    [SerializeField]
+    private bool isPaused;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivateMenu();
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            DeactivateMenu();
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+}
---
+++ Assets/Scripts/SpecificObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecificObject : SaveableObject
+{
+    public override void Save(int id)
+    {
+        base.Save(id);
+    }
+    public override void Load(string[] values)
+    {
+        base.Load(values);
+    }
+}
+
