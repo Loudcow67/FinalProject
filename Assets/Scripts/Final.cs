5dd31ebf8f539001b655f1253ff1217b42de1b5c
Loudcow67 <logan.soulliere@ontariotechu.net>
2020-11-15 11:56:18 -0500

    Finsih Line Added

---
+++ Assets/Scripts/Final.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Final : MonoBehaviour
+{
+    public GameObject panel;
+    public Text finalTime;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            panel.SetActive(true);
+            finalTime.text = GetComponent<DllManager>().LoadTotalTime().ToString() + " Seconds";
+        }
+    }
+}
